60cca1c66ad9327e3dbf75c0c24c968e1aeb3bf4
Jose Sanchez <josedavid_san@hotmail.com>
2023-09-11 21:53:22 -0500

    base proyecto

---
+++ app/Producto.py
@@ -0,0 +1,7 @@
+class Producto:
+    def __init__(self):
+        self.nombre = ""
+        self.cantidad = 0
+        self.cantidad_minima = ""
+        self.correo_provedor = ""
+        self.stock_pedido = 0
